00a1c3b24d377f5f4347ba55bc348f11ca91bccd
masaki-shimura <operationsim.game@gmail.com>
2024-02-28 07:31:13 +0900

    Add 各種サンプルコードの追加

--- UnitySampleUnitTest/Assets/Scripts/Editor/HogeTest.cs
+++ UnitySampleUnitTest/Assets/Scripts/Editor/HogeTest.cs
@@ -1,25 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
 public class HogeTest
 {
-    // A Test behaves as an ordinary method
-    [Test]
+    [Test,Category("正常系テスト"),Description("サンプルテストコード Debug.Logでもログを残せる！")]
     public void HogeTestSimplePasses()
     {
-        // Use the Assert class to test conditions
+        var hoge = new Hoge();
+        var equalToValue = 2;
+        var result = hoge.Add(1, 1);
+        
+        Assert.That(result,Is.EqualTo(equalToValue),"失敗しました");
+        
+        Debug.Log($"result:{result}/equalToValue:{equalToValue}");
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator HogeTestWithEnumeratorPasses()
+    [Test,Category("正常系テスト")]
+    [Ignore("メソッド内でエラーが発生した時にどのような挙動がされるのか確認する為のテストコード")]
+    public void HogeTestForceExceptionTest()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        var hoge = new Hoge();
+        hoge.ForceException();
+    }
+    
+    [Test,Category("異常系テスト"),Description("Exceptionが呼ばれたら成功するサンプルコード")]
+    public void HogeTestForceExceptionSuccessTest()
+    {
+        var hoge = new Hoge();
+        Assert.That(()=>
+        {
+            hoge.ForceException();
+        }, Throws.TypeOf<Exception>(),"Exceptionが呼ばれてないよー");
+    }
+    [Test,Category("正常系テスト"),Description("色んなパターンで実行")]
+    [TestCase(0,1,1)]
+    [TestCase(0,1,2)]
+    [TestCase(0,2,1)]
+    public void HogeTestSimpleVariousSuccessPasses(int greaterValue,int a,int b)
+    {
+        var hoge = new Hoge();
+        var result = hoge.Add(a, b);
+        
+        Assert.That(result,Is.GreaterThan(greaterValue),"失敗しました");
+        
+        Debug.Log($"result:{result}/equalToValue:{greaterValue}");
+    }
+    
+    [Test,Category("失敗例"),Description("色んなパターンで実行 失敗例"),Ignore("失敗例")]
+    [TestCase(0,1,1)]
+    [TestCase(0,1,-2)]
+    [TestCase(0,2,1)]
+    public void HogeTestSimplePassesFailure(int greaterValue,int a,int b)
+    {
+        var hoge = new Hoge();
+        var result = hoge.Add(a, b);
+        
+        Assert.That(result,Is.GreaterThan(greaterValue),"失敗しました");
+        
+        Debug.Log($"result:{result}/equalToValue:{greaterValue}");
     }
 }
---
+++ UnitySampleUnitTest/Assets/Scripts/Hoge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DefaultNamespace
+{
+    public sealed class Hoge
+    {
+        public int Add(int a , int b)
+        {
+            
+            return a + b;
+        }
+        
+        /// <summary>
+        /// 強制的にExceptionを実行する
+        /// </summary>
+        public void ForceException()
+        {
+            throw new Exception();
+        }
+    }
+}
